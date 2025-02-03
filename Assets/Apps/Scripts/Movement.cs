a42041ce8367c854aa2eaf731ef1cf18838a751a
FMPanggabean <cichio.maru@gmail.com>
2025-02-03 11:49:42 +0700

    add movement feature
    add setdirection function

--- Assets/Apps/Scripts/Movement.cs
+++ Assets/Apps/Scripts/Movement.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Range(5f, 20f)] private float speed;
+
+    private Rigidbody2D rb2d;
+    private float direction;
+
+    private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        if (rb2d is null)
+        {
+            gameObject.AddComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        Vector2 velocity = rb2d.velocity;
+        velocity.x = direction * speed;
+        rb2d.velocity = velocity;
+    }
+
+    public void SetDirection(float direction)
+    {
+        this.direction = direction;
     }
 }
